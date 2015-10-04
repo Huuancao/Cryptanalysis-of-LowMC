6942c00ec05f639b85c1e7d0ec2686c73eccf790
Huuancao <huuan.cao@gmail.com>
2015-10-04 12:56:03 +0200

    Generate subspaces
    Generate base and subspaces.

--- Basic Attack Implementation/BasAttImp.cpp
+++ Basic Attack Implementation/BasAttImp.cpp
@@ -12,6 +12,7 @@ const unsigned keysize = 6; // Key size in bits
 const unsigned rounds = 6; // Number of rounds
 const unsigned tail = 7; // Number of bits in tail
 const unsigned dimension = 12; //Dimension of vector space
+const unsigned maxpermut= 4080;
 
 const string plainPath = "../LowMC/plaintexts.txt";
 const string cipherPath = "../LowMC/ciphertexts.txt";
@@ -29,13 +30,33 @@ typedef std::bitset<dimension> vecspace;
 //   FUNCTIONS  //
 //////////////////
 /*
-Generate combination C(12,8) subspaces
+Generate next lexicographically permutation.
 */
-void setSubspaces(std::vector<vecspace>& subspaces){
+unsigned int nextPermut(unsigned int currentPermut){
+    unsigned int v(currentPermut); // current permutation of bits 
+    unsigned int w; // next permutation of bits
 
+    unsigned int t = v | (v - 1); // t gets v's least significant 0 bits set to 1
+    // Next set to 1 the most significant bit to change, 
+    // set to 0 the least significant ones, and add the necessary 1 bits.
+    w = (t + 1) | (((~t & -~t) - 1) >> (__builtin_ctz(v) + 1));  
+    vecspace v1(v);
+    vecspace v2(w);
+    //cout << "current: " << v1 << " next: " << v2 << endl;
+    return w;
 }
 /*
-Generate vector space 12x16
+Generate C(12,8) subspaces.
+*/
+void setSubspaces(vector<vecspace>& subspaces){
+    unsigned int current(255);
+    do {
+            subspaces.push_back(current);
+            current = nextPermut(current);
+    }while(current <= maxpermut);
+}
+/*
+Generate vector space 12x16.
 */
 void setVectorSpace(std::vector<block>& base){
     block tempVector(0);
@@ -47,9 +68,18 @@ void setVectorSpace(std::vector<block>& base){
 }
 
 /*
-Print vector of sequences of bitsets.
+Print vector of sequences of blocks.
 */
-void printSequences(std::vector<block>& sequences){
+void printSequencesBlocks(std::vector<block>& sequences){
+    for(int i=0; i< sequences.size(); ++i){
+        cout << "Entry n" <<i << ": " << sequences[i] << endl;
+    }
+}
+
+/*
+Print vector of sequences of vecspaces.
+*/
+void printSequencesVecspaces(std::vector<vecspace>& sequences){
     for(int i=0; i< sequences.size(); ++i){
         cout << "Entry n" <<i << ": " << sequences[i] << endl;
     }
@@ -91,11 +121,13 @@ int main(int argc, const char * argv[]) {
     vector<block> ciphertexts;
     vector<block> base;
     vector<vecspace> subspaces;
-    //initInputs(plaintexts, plainPath);
-    //initInputs(ciphertexts, cipherPath);
+    initInputs(plaintexts, plainPath);
+    initInputs(ciphertexts, cipherPath);
     setVectorSpace(base);
-
+    setSubspaces(subspaces);
     
+
+    //printSequencesVecspaces(subspaces);
     //printSequences(base);
     //printSequences(plaintexts);
     //printSequences(ciphertexts);
